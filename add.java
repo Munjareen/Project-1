af6384f0e7023a0d733d8a58681b7a06892da32a
Munjareen M <mmunjareen@gmail.com>
2025-07-16 15:52:02 +0530

    num5 changed into num10

--- add.java
+++ add.java
@@ -9,13 +9,13 @@ public class add {
         int num1 = scanner.nextInt();
 
         System.out.print("Enter the second number: ");
-        int num5 = scanner.nextInt();
+        int num10 = scanner.nextInt();
 
         // Calculate the sum
-        int sum = num1 + num5;
+        int sum = num1 + num10;
 
         // Display the result
-        System.out.println("The sum of " + num1 + " and " + num5 + " is: " + sum);
+        System.out.println("The sum of " + num1 + " and " + num10 + " is: " + sum);
 
         scanner.close();
     }
